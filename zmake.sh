ba24cd264f62460235a5b74ee26bde9b2d191190
Ant-ON <prozanton@gmail.com>
2011-02-08 20:40:46 +0500

    Change build script

--- zmake.sh
+++ zmake.sh
@@ -1,9 +1,71 @@
 #!/bin/bash
+
+MAKE()
+{
+rm build/.libs/libSDL-1.2.so.0.11.3
+make
+$STRIP build/.libs/libSDL-1.2.so.0.11.3
+ncftpput -u root -p "" 192.168.16.2 /mmc/mmca1 build/.libs/libSDL-1.2.so.0.11.3
+}
+
 source ezxenv.sh
+
+makefrom=$1
 
 $MOC src/video/magx/SDL_magx_win.h -o src/video/magx/moc_SDL_magx_win.cc
 
-make
-$STRIP build/.libs/libSDL-1.2.so.0.11.3
+if ["$makefrom" == ""]
+then
+makefrom=ve66
+fi
 
-ncftpput -u root -p "" 192.168.16.2 /mmc/mmca1 build/.libs/libSDL-1.2.so.0.11.3
+case "$makefrom" in
+	ve66)
+		export QTDIR=/arm-eabi/lib/qt-ve66
+		export EZXDIR=/arm-eabi/lib/ezx-ve66
+		export MOD=VE66_EM35
+		MAKE
+		;;
+	zn5)
+		export QTDIR=/arm-eabi/lib/qt-zn5
+		export EZXDIR=/arm-eabi/lib/ezx-zn5
+		export MOD=ZN5_U9
+		MAKE
+		;;
+	e8)
+		export QTDIR=/arm-eabi/lib/qt-e8
+		export EZXDIR=/arm-eabi/lib/ezx-e8
+		export MOD=E8
+		MAKE
+		;;
+	em30)
+		export QTDIR=/arm-eabi/lib/qt-em30
+		export EZXDIR=/arm-eabi/lib/ezx-em30
+		export MOD=EM30
+		MAKE
+		;;
+	va76)
+		export QTDIR=/arm-eabi/lib/qt-e8
+		export EZXDIR=/arm-eabi/lib/ezx-va76
+		export MOD=VA76
+		MAKE
+		;;
+	z6w)
+		export QTDIR=/arm-eabi/lib/qt-z6w
+		export EZXDIR=/arm-eabi/lib/ezx-z6w
+		export MOD=Z6W
+		MAKE
+		;;
+	z6)
+		export QTDIR=/arm-eabi/lib/qt-z6
+		export EZXDIR=/arm-eabi/lib/ezx-z6
+		export MOD=Z6
+		MAKE
+		;;
+	v8)
+		export QTDIR=/arm-eabi/lib/qt-v8
+		export EZXDIR=/arm-eabi/lib/ezx-v8
+		export MOD=V8
+		MAKE
+		;;
+esac
---
+++ zmake_pak.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+MAKE()
+{
+make clean
+rm build/.libs/libSDL-1.2.so.0.11.3
+make
+$STRIP build/.libs/libSDL-1.2.so.0.11.3
+cp build/.libs/libSDL-1.2.so.0.11.3 libSDL-1.2.so.0
+7z a libSDL-1.2_$VER\_$MOD.7z  libSDL-1.2.so.0 
+rm libSDL-1.2.so.0
+}
+
+source ezxenv.sh
+export VER=C1
+
+makefrom=$1
+
+$MOC src/video/magx/SDL_magx_win.h -o src/video/magx/moc_SDL_magx_win.cc
+
+if [ "$makefrom" == "" ]
+then
+makefrom=all
+fi
+
+if [ "$makefrom" == "ve66" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-ve66
+export EZXDIR=/arm-eabi/lib/ezx-ve66
+export MOD=VE66_EM35
+MAKE
+fi
+
+if [ "$makefrom" == "zn5" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-zn5
+export EZXDIR=/arm-eabi/lib/ezx-zn5
+export MOD=ZN5_U9
+MAKE
+fi
+
+if [ "$makefrom" == "e8" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-e8
+export EZXDIR=/arm-eabi/lib/ezx-e8
+export MOD=E8
+MAKE
+fi
+
+if [ "$makefrom" == "em30" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-em30
+export EZXDIR=/arm-eabi/lib/ezx-em30
+export MOD=EM30
+MAKE
+fi
+
+if [ "$makefrom" == "va76" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-e8
+export EZXDIR=/arm-eabi/lib/ezx-va76
+export MOD=VA76
+MAKE
+fi
+
+if [ "$makefrom" == "z6w" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-z6w
+export EZXDIR=/arm-eabi/lib/ezx-z6w
+export MOD=Z6W
+MAKE
+fi
+
+if [ "$makefrom" == "z6" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-z6
+export EZXDIR=/arm-eabi/lib/ezx-z6
+export MOD=Z6
+MAKE
+fi
+
+if [ "$makefrom" == "v8" ] || [ "$makefrom" == "all" ]
+then
+export QTDIR=/arm-eabi/lib/qt-v8
+export EZXDIR=/arm-eabi/lib/ezx-v8
+export MOD=V8
+MAKE
+fi
